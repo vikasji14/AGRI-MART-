5e7b8ac193564dfefaa1835134c5a042da6278fc
githubinfovikas <vikasdbg453@gmail.com>
2024-08-15 11:22:32 +0530

    chore: Update MongoDB connection string in config/db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 import colors from "colors";
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb+srv://admin:admin@cluster0.puvdo76.mongodb.net/?retryWrites=true&w=majority");
-    // const conn = await mongoose.connect('mongodb+srv://vikasji14:vikasji14@database1.9ulraxk.mongodb.net/')
+    // const conn = await mongoose.connect("mongodb+srv://admin:admin@cluster0.puvdo76.mongodb.net/?retryWrites=true&w=majority");
+    const conn = await mongoose.connect('mongodb+srv://vikasji14:vikasji14@database1.9ulraxk.mongodb.net/')
     console.log(
       `Conneted To Mongodb Databse ${conn.connection.host}`.bgMagenta.white
     );
